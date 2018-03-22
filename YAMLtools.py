499a5322b1933ce12163d9861d08a28d2b01ef7c
duankaikai <duankk@pmo.ac.cn>
2018-03-22 21:03:47 +0800

    update AddPointSource AddDiffuseSource DelSource

--- YAMLtools.py
+++ YAMLtools.py
@@ -2,9 +2,69 @@ import yaml
 from pprint import pprint
 from collections import Counter
 
-class YAMLmodel(object):
+class NewModel(object):
+    def SaveModel(self, outfile):
+        with open(outfile, 'w') as f:
+            yaml.dump(self.modelDict, f)
+    
     def __init__(self, filename):
-        self.filename = filename.split('.yaml')[0]
+        self.filename = filename
+        self.modelDict = {}
+
+    def AddPointSource(self, srcName, SpectralType, SpectralPars, skycrd_C):
+        ra = str(skycrd_C[0])
+        dec = str(skycrd_C[1])
+        spectrum = {'type':SpectralType}
+        for parName, parDict in SpectralPars.items():
+            free = str(parDict['free'])
+            scale = str(parDict['scale'])
+            value = str(parDict['value'])
+            parmin = str(parDict['min'])
+            parmax = str(parDict['max'])
+            spectrum[parName] = {'free':free, 'name':parName, 'max':parmax, 'min':parmin, 'scale':scale, 'value':value}
+
+        source = {'name': srcName,
+                  'type': 'PointSource',
+                  'spectrum': spectrum,
+                  'spatialModel': {'type': 'SkyDirFunction',
+                                     'RA': {'free':'0', 'max':'360.', 'min':'-360.', 'scale':'1', 'value':ra},
+                                    'DEC': {'free':'0', 'max':'90.', 'min':'-90.', 'scale':'1', 'value':dec}
+                                  }
+                 }
+        self.modelDict[srcName] = source
+        self.SaveModel(self.filename)
+
+    def AddDiffuseSource(self, srcName, SpectralType, SpectralPars, SpatialType, SpatialFile, SpatialPar):
+        spectrum = {'type':SpectralType}
+        for parName, parDict in SpectralPars.items():
+            free = str(parDict['free'])
+            scale = str(parDict['scale'])
+            value = str(parDict['value'])
+            parmin = str(parDict['min'])
+            parmax = str(parDict['max'])
+            spectrum[parName] = {'free':free, 'name':parName, 'max':parmax, 'min':parmin, 'scale':scale, 'value':value}
+
+        name = SpatialPar['name']
+        free = str(SpatialPar['free'])
+        scale = str(SpatialPar['scale'])
+        value = str(SpatialPar['value'])
+        parmin = str(SpatialPar['min'])
+        parmax = str(SpatialPar['max'])
+        spatialModel = {'type': SpatialType, 'file': SpatialFile}
+        spatialModel[name] = {'free':free, 'name':name, 'min':parmin, 'max':parmax, 'scale':scale, 'value':value}
+
+        source = {'name': srcName,
+                  'type': 'DiffuseSource',
+                  'spectrum': spectrum,
+                  'spatialModel': spatialModel}
+        self.modelDict[srcName] = source
+        self.SaveModel(self.filename)
+
+
+class LoadModel(NewModel):
+    def __init__(self, filename):
+        self.basename = filename.split('.yaml')[0]
+        self.filename = filename
         with open(filename, 'r') as f:
             self.modelDict = yaml.load(f)
 
@@ -41,8 +101,8 @@ class YAMLmodel(object):
                     except:
                         pass
 
-        self.NumDict = dict(Counter(self.SrcList))
-        self.SrcList = list(self.NumDict.keys())
+        self.ParsNumDict = dict(Counter(self.SrcList))
+        self.SrcList = list(self.ParsNumDict.keys())
         self.FreeNumDict = dict(Counter(self.FreeSrcList))
         self.FreeSrcList = list(self.FreeNumDict.keys())
 
@@ -86,29 +146,44 @@ class YAMLmodel(object):
 
     def SetParScaledValue(self, srcName, parName, value):
         self.modelDict[srcName]['spectrum'][parName]['value'] = str(value)
-        outfile = self.filename + '_SetParScaledValue_{}_{}_{}.yaml'.format(srcName, parName, value)
-        with open(outfile, 'w') as f:
-            yaml.dump(self.modelDict, f)
-        return self.__init__(outfile)
+        outfile = self.basename + '_SetParScaledValue_{}_{}_{}.yaml'.format(srcName, parName, value)
+        self.SaveModel(outfile)
 
     def SetParScale(self, srcName, parName, scale):
         self.modelDict[srcName]['spectrum'][parName]['scale'] = str(scale)
-        outfile = self.filename + '_SetParScale_{}_{}_{}.yaml'.format(srcName, parName, scale)
-        with open(outfile, 'w') as f:
-            yaml.dump(self.modelDict, f)
-        return self.__init__(outfile)
+        outfile = self.basename + '_SetParScale_{}_{}_{}.yaml'.format(srcName, parName, scale)
+        self.SaveModel(outfile)
 
     def SetParFree(self, srcName, parName, free):
         self.modelDict[srcName]['spectrum'][parName]['free'] = str(free)
-        outfile = self.filename + '_SetParFree_{}_{}_{}.yaml'.format(srcName, parName, free)
-        with open(outfile, 'w') as f:
-            yaml.dump(self.modelDict, f)
-        return self.__init__(outfile)
+        outfile = self.basename + '_SetParFree_{}_{}_{}.yaml'.format(srcName, parName, free)
+        self.SaveModel(outfile)
+
+    def DelSource(self, srcName):
+        del self.modelDict[srcName]
+        outfile = self.basename + '_DelSource_{}.yaml'.format(srcName)
+        self.SaveModel(outfile)
 
 
 if __name__ == '__main__':
-    modelfile = 'YAMLmodel.yaml'
-    model = YAMLmodel(modelfile)
+    filename = 'myYAMLmodel.yaml'
+    myModel = NewModel(filename)
+
+    SpectralType = 'PowerLaw'
+    SpectralPars = {'Prefactor': {'free':1, 'max':1000, 'min':0.001, 'scale':1e-9, 'value':1},
+                        'Index': {'free':1, 'max':5, 'min':1, 'scale':-1, 'value':2},
+                        'Scale': {'free':0, 'max':2000, 'min':30, 'scale':1, 'value':100}}
+    skycrd_C = (83.45, 21.72)
+    myModel.AddPointSource('myPowerLaw_source', SpectralType, SpectralPars, skycrd_C)
+
+    SpatialType = 'SpatialMap'
+    SpatialFile = 'SpatialMap_source.fits'
+    SpatialPar = {'name': "Normalization", 'free':1, 'min':0.001, 'max':1000, 'scale':1, 'value':100}
+    myModel.AddDiffuseSource('myDiffuse_source', SpectralType, SpectralPars, SpatialType, SpatialFile, SpatialPar)
+
+    filename = 'YAMLmodel.yaml'
+    model = LoadModel(filename)
+    model.AddPointSource('myPowerLaw_source', SpectralType, SpectralPars, skycrd_C)
 
     pprint(model.SrcList)
     pprint(model.FixSrcList)
@@ -116,7 +191,7 @@ if __name__ == '__main__':
     pprint(model.ParList)
     pprint(model.FixParList)
     pprint(model.FreeParList)
-    pprint(model.NumDict)
+    pprint(model.ParsNumDict)
     pprint(model.FreeNumDict)
 
     srcName = 'PowerLaw_source'
@@ -134,3 +209,5 @@ if __name__ == '__main__':
     model.SetParScaledValue(srcName, parName, 1)
     model.SetParScale(srcName, parName, 1e-10)
     model.SetParFree(srcName, parName, 0)
+
+    model.DelSource('myPowerLaw_source')
